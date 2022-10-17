4b91c2ba81cd83d5cbdadb7d563d7e1538743b47
Arnaud Boulianne <arnaud@profasee.com>
2022-10-17 16:49:50 -0400

    Initial revision

---
+++ src/main/java/com/rndbblnn/stonks/commons/autoconfigure/CommonsAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.rndbblnn.stonks.commons.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+//@ConditionalOnProperty(prefix = "handlebars", value = "enabled", havingValue = "true", matchIfMissing = true)
+@Configuration
+public class CommonsAutoConfiguration {
+
+}
