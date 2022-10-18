7c4045333f0e422cde70e10c11793f0feb8161d2
Arnaud Boulianne <arnaud@profasee.com>
2022-10-18 10:43:50 -0400

    Added entity

---
+++ src/main/java/com/rndbblnn/stonks/commons/entity/Candle1mEntity.java
@@ -0,0 +1,12 @@
+package com.rndbblnn.stonks.commons.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.ToString;
+
+@Entity
+@Table(name = "candle_1m")
+@ToString
+public class Candle1mEntity extends BaseCandle {
+
+}
